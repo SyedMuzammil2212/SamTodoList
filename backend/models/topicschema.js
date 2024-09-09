9598f437a2696bc28d9ed39fe0976f76206ada85
syedabdulmuzammil <abdulmuzammil@devsed.com>
2024-09-09 20:52:12 +0530

    push

--- backend/models/topicschema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const topic = new mongoose.Schema({
-  name: String,
-  age: Number,
-  data: String,
-});
-
-module.exports = mongoose.model("topic", topic);
